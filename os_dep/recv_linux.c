2b6f71e89a5be5d7c0008d9f097a634288aa6000
Denis Klester <dinisoft@gmail.com>
2025-08-03 18:59:32 +0600

    fix build for 6.16

--- os_dep/recv_linux.c
+++ os_dep/recv_linux.c
@@ -607,10 +607,12 @@ static void _rtw_reordering_ctrl_timeout_handler(struct timer_list *t)
 static void _rtw_reordering_ctrl_timeout_handler (void *FunctionContext)
 #endif
 {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
+	struct recv_reorder_ctrl *preorder_ctrl = (struct recv_reorder_ctrl *)FunctionContext;
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0)
 	struct recv_reorder_ctrl *preorder_ctrl = from_timer(preorder_ctrl, t, reordering_ctrl_timer);
 #else
-	struct recv_reorder_ctrl *preorder_ctrl = (struct recv_reorder_ctrl *)FunctionContext;
+	struct recv_reorder_ctrl *preorder_ctrl = timer_container_of(preorder_ctrl, t, reordering_ctrl_timer);
 #endif
 	rtw_reordering_ctrl_timeout_handler(preorder_ctrl);
 }
